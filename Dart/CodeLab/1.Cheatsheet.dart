076d6c3bb1e06fa4728766d15cb78226851df67a
Abdullah Alsabouni <98663047+abdullah012345678910@users.noreply.github.com>
2023-11-07 14:53:55 +0300

    Create 1.Cheatsheet.dart

---
+++ Dart/CodeLab/1.Cheatsheet.dart
@@ -0,0 +1,30 @@
+
+// String? name = 'Jane'; // Geçersiz veya null 
+// String? address;
+
+void main() {
+//   int? a; // = null
+//   a ??= 3; // a Değişkeni boşsa a = 3
+//   print(a); // ==> 3
+  
+//   a ??= 5; // a değişkeni boş olmadığı için 5 değeri a'ya atanmaz
+//   print(a); // ==> 3
+//   print(18 ?? 3); // ==> 18 ?? Operatörü null yoksa herzaman soldaki değeri alır
+//   print(null ?? 12); // 12
+//   print(null ?? null); // null
+ // myObject?.someProperty // myObject null'sa .someProperty nesnesini ata // (myObject != null) ? myObject.someProperty : null ==> Aynı sonuç
+  
+  
+ // This method should return the uppercase version of `str`
+// or null if `str` is null.
+  
+  
+  
+// String? upperCaseIt(String? str) {
+  
+ //  return str?.toUpperCase(); // str null'sa null toUpperCase() metodunu çalıştır
+   
+ // }
+  
+ }
+ 
